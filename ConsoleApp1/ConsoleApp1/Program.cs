1a96b35a0c169d4288aa7129f7b566f7aa1c2f53
CarlosFR <carlos.flores.romay.7@gmail.com>
2023-08-03 15:54:07 -0400

    IEnumerable

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    using System;
+    using System.Collections;
+
+    // Simple business object.
+    public class Person
+    {
+        public Person(string fName, string lName)
+        {
+            this.firstName = fName;
+            this.lastName = lName;
+        }
+
+        public string firstName;
+        public string lastName;
+    }
+
+    // Collection of Person objects. This class
+    // implements IEnumerable so that it can be used
+    // with ForEach syntax.
+    public class People : IEnumerable
+    {
+        private Person[] _people;
+        public People(Person[] pArray)
+        {
+            _people = new Person[pArray.Length];
+
+            for (int i = 0; i < pArray.Length; i++)
+            {
+                _people[i] = pArray[i];
+            }
+        }
+
+        // Implementation for the GetEnumerator method.
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
+
+        public PeopleEnum GetEnumerator()
+        {
+            return new PeopleEnum(_people);
+        }
+    }
+
+    // When you implement IEnumerable, you must also implement IEnumerator.
+    public class PeopleEnum : IEnumerator
+    {
+        public Person[] _people;
+
+        // Enumerators are positioned before the first element
+        // until the first MoveNext() call.
+        int position = -1;
+
+        public PeopleEnum(Person[] list)
+        {
+            _people = list;
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < _people.Length);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public Person Current
+        {
+            get
+            {
+                try
+                {
+                    return _people[position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+    }
+
+    public class MyClass : IEnumerable<double>
+    {
+        public MyClass()
+        {
+            myData = new List<double>(new double[] { 3.4, 1.2, 6.2 });
+        }
+
+        private List<double> myData;
+
+        public IEnumerator<double> GetEnumerator()
+        {
+            foreach (double val in myData)
+            {
+                yield return val;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] peopleArray = new Person[3]
+        {
+            new Person("John", "Smith"),
+            new Person("Jim", "Johnson"),
+            new Person("Sue", "Rabon"),
+        };
+
+            People peopleList = new People(peopleArray);
+            foreach (Person p in peopleList)
+                Console.WriteLine(p.firstName + " " + p.lastName);
+
+            IEnumEx2();
+
+            Console.ReadLine();
+        }
+
+        private static void IEnumEx2()
+        {
+            MyClass myClass = new MyClass();
+            List<double> myList = new List<double>();
+            foreach (var item in myClass)
+            {
+                myList.Add(item * 2);
+            }
+        }
+    }
+}
